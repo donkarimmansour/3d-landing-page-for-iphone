7175367ac6b8acda08313b0b0a82fec9e27dac6c
karim wydadi <don.karimmansour@gmail.com>
2022-12-03 23:16:17 +0000

    'jjj'

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,29 @@
 import { NextSeo } from "next-seo"
+import { useRouter } from "next/router"
 import propTypes from 'prop-types'
+import { useEffect } from "react"
 import { useTranslation } from "react-i18next"
 
 const FourZeroFour = () => {
 
-  const { t } = useTranslation('common')
+ // const { t } = useTranslation('common')
 
   const SEO = {
     title: "Next website",  
     description: "Database of movies created with Next.js and Strapi",
   }
 
+  const { locale, pathname, query, asPath , push} = useRouter();
+
+  useEffect(() => {
+     push("/")
+    // push({ pathname, query }, asPath, { locale: locale })
+  } , [])
+ 
+
   return (
     <>
-     
+      
      <NextSeo {...SEO } />
       not found
     </>
@@ -26,13 +36,13 @@ FourZeroFour.propTypes = {
 }
  
 
+// FourZeroFour.getInitialProps = async () => {
+//   return { 
+//     namespacesRequired: ['common']
+//   }
+// }
+
 export default FourZeroFour
  
-
-FourZeroFour.getInitialProps = async () => {
-  return { 
-    namespacesRequired: ['common']
-  }
-}
 
 
